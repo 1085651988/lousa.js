7e6d70ce1e14f5dca8bc75e68419112fef04e657
1085651988 <00001085651988sp@al.educacao.sp.gov.br>
2024-05-27 20:53:29 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400,400);
+  background("black");
+}
+function draw(){
+  stroke("blue");
+  fill("red");
+
+  if(mouselsPressed){
+   rect(mouseX,mouseY,20,35);
+  }
+}
